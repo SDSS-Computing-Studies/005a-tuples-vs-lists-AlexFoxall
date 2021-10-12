a93b5928967f565448f51cf849be1dbbbf563810
Alex <90716207+AlexFoxall@users.noreply.github.com>
2021-10-12 14:38:16 -0700

    done

--- task3.py
+++ task3.py
@@ -15,5 +15,8 @@ The smallest number is 3 and the largest number is 9
 
 myList = [ 3,6,5,4,6,7,8,6,5,9,4,5 ]
 myList = sorted(myList)
-print(myList[:1])
-print(myList[11:])
+t1 = (myList[0])
+t2 = (myList[11])
+t1 = str(t1)
+t2 = str(t2)
+print("The smallest number is " + t1 + " and the largest number is " + t2)
